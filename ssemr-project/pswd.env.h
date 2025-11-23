e7d31a3026f22b88bc5e2c1f02c8aed227c0ad7a
Lalith-888 <lalith.j_lgp25@ashoka.edu.in>
2025-11-23 10:58:37 +0530

    Add files via upload

---
+++ ssemr-project/pswd.env.h
@@ -0,0 +1,6 @@
+#define BLYNK_TEMPLATE_ID "TMPL3lrMknAjW"
+#define BLYNK_TEMPLATE_NAME "SSEMR Template"
+#define BLYNK_AUTH_TOKEN "ZunEc86KlKa-2ut8B3KW-DbPJ35CMgGk"
+
+#define WIFI_SSID "scarly4g"
+#define WIFI_PASS "nuthana25"
